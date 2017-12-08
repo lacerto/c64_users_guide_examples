ddd1b048f577af3c8e168a70f37e814f16bab2aa
lacerto <ladislaus@zoho.com>
2017-12-08 15:04:19 +0100

    Add comments to color bars example

--- src/color-bars.s
+++ src/color-bars.s
@@ -4,39 +4,39 @@
 
 ; *** labels ***
 
-givayf          = $b391
-getadr          = $b7f7
-fmul            = $ba28
-strout          = $ab1e
-rnd             = $e097
+givayf          = $b391         ; 16-bit signed int to FAC1
+getadr          = $b7f7         ; FAC1 to unsigned 16-bit int
+fmul            = $ba28         ; load float from a/y then FAC1=FAC1*(a/y)
+strout          = $ab1e         ; print null terminated string
+rnd             = $e097         ; basic's rnd function
 
 ; *** main ***
 
                 *=$02a8         ; sys 680
 
-                lda #<clr
+                lda #<clr       ; clear the screen
                 ldy #>clr
                 jsr strout
 mainloop                
-                ldy #$01
+                ldy #$01        ; load 1 into FAC1
                 lda #$00
                 jsr givayf
-                jsr rnd
-                lda #<const8
-                ldy #>const8
+                jsr rnd         ; call rnd with FAC1 as its argument
+                lda #<const8    ; rnd's result is also in FAC1, a floating point value between 0-1
+                ldy #>const8    ; multiply this value by 8
                 jsr fmul
-                jsr getadr
-                lda colortable, y
-                sta bar
-                lda #<bar
+                jsr getadr      ; convert the result in FAC1 to int
+                                ; y is the low byte; should contain a number between 0-7
+                lda colortab, y ; index the color table using y    
+                sta bar         ; store the value from the table
+                lda #<bar       ; print the resulting string
                 ldy #>bar
                 jsr strout
-                jmp mainloop
-                rts
+                jmp mainloop    ; loop forever
 
 ; *** data ***
 
-clr             .byte $93, $00
+clr             .byte $93, $00  ; clr (clear screen) control code
 
 bar             .byte $20       ; placeholder for color control code
                 .byte $12       ; reverse on
@@ -44,7 +44,8 @@ bar             .byte $20       ; placeholder for color control code
                 .byte $92       ; reverse off
                 .byte $00       ; string terminator
 
-colortable      .byte $05, $1c, $1e, $1f, $90, $9c, $9e, $9f
+                ; white, red, green, blue, black, purple, yellow, cyan
+colortab        .byte $05, $1c, $1e, $1f, $90, $9c, $9e, $9f
 
-const8          .byte $84
+const8          .byte $84       ; a constant 8 as a 5-byte floating point number
                 .repeat 4, $00
